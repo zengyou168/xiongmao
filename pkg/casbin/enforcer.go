18d1971a456b69b1cc5c63307f180ddb129faa67
zengyou <15989788839@qq.com>
2024-08-02 17:10:59 +0800

    .

--- pkg/casbin/enforcer.go
+++ pkg/casbin/enforcer.go
@@ -1,33 +1,33 @@
 package casbin
 
 import (
-    "github.com/casbin/casbin/v2"
-    gormadapter "github.com/casbin/gorm-adapter/v3"
-    "xiongmao/pkg/db"
-    "xiongmao/pkg/log"
+	"github.com/casbin/casbin/v2"
+	gormadapter "github.com/casbin/gorm-adapter/v3"
+	"xiongmao/pkg/db"
+	"xiongmao/pkg/log"
 )
 
 var EnforcerVar *casbin.Enforcer
 
 func Init() {
 
-    a, err := gormadapter.NewAdapterByDB(db.Gorm)
+	a, err := gormadapter.NewAdapterByDB(db.Gorm)
 
-    if err != nil {
-        log.SugarVar.Error("casbin适配器初始化失败")
-    }
+	if err != nil {
+		log.SugarVar.Error("casbin适配器初始化失败")
+	}
 
-    e, err := casbin.NewEnforcer("pkg/casbin/model.conf", a)
+	e, err := casbin.NewEnforcer("pkg/casbin/model.conf", a)
 
-    if err != nil {
-        log.SugarVar.Error("casbin执行器初始化失败", err)
-    }
+	if err != nil {
+		log.SugarVar.Error("casbin执行器初始化失败", err)
+	}
 
-    err = e.LoadPolicy()
+	err = e.LoadPolicy()
 
-    if err != nil {
-        log.SugarVar.Error("casbin策略加载失败", err)
-    }
+	if err != nil {
+		log.SugarVar.Error("casbin策略加载失败", err)
+	}
 
-    EnforcerVar = e
+	EnforcerVar = e
 }
